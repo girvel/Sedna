bcdf74695d7bde9cac383d0647b81c7491df3142
girvel <widauka@ya.ru>
2018-02-22 21:42:02 +0500

    realized testing of create_cube(...)

--- geometry/model.py
+++ geometry/model.py
@@ -4,14 +4,14 @@ from geometry.vector import Vector
 
 class Model:
     def __init__(self, dimensions, primitives, position=None):
-        self.__primitives = primitives
+        self._primitives = primitives
         self.position = Vector.get_zero_vector(dimensions) if position is None else position
 
     def get_final_primitives(self):
-        return [Primitive(p.line + self.position, p.color, p.arrow) for p in self.__primitives]
+        return [Primitive(p.line + self.position, p.color, p.arrow) for p in self._primitives]
 
     def rotate(self, dim1, dim2, angle):
-        for p in self.__primitives:
+        for p in self._primitives:
             p.line.v1 = p.line.v1.rotated(dim1, dim2, angle)
             p.line.v2 = p.line.v2.rotated(dim1, dim2, angle)
 
---
+++ geometry_tests/cube_tests.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest import TestCase
+
+from geometry.cube import create_cube
+from geometry.vector import Vector
+
+
+class CubeTests(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create_cube(self):
+        def lines_number(d):
+            return 1 if d == 1 else 2 * lines_number(d - 1) + 2**(d - 1)
+
+        # arrange
+        position = Vector(1, 1, 1, 1, 1)
+        dimensions = 5
+
+        # act
+        cube = create_cube(2, dimensions, position=position)
+
+        # assert
+        self.assertEqual(cube.position, position)
+        self.assertEqual(len(cube._primitives), lines_number(dimensions))
+
+    def test_strings_a_3(self):
+        self.assertEqual('a' * 3, 'aaa')
+
+
+if __name__ == '__main__':
+    unittest.main()
